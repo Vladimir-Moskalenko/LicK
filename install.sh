6d93a0855da52efd0e6b077842bfbdcfaca48ef2
Vladimir Moskalenko <93604107+Vladimir-Moskalenko@users.noreply.github.com>
2024-09-13 08:37:42 -0700

    Update 3: Even more icons!
    Full /System/Applications support, plus Finder!

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 # Made by \/\\\
+dir=$PWD
 
 installAquaTheme()
 {
@@ -37,15 +38,15 @@ sudo bless --mount "$HOME/lvmnt/System/Library/CoreServices/" --setBoot --create
 echo Installation successful! Reboot to apply changes.
 }
 
-installIcon()
+installSysIcon()
 {
-	echo Installing $1 icon...
-	./fileicon ~/lvmnt/System/Applications/$1.app /icons/$1
+	cd $PWD
+	echo Installing "$1" icon...
+	sudo ./fileicon set ~/lvmnt/System/Applications/"$1".app SystemIcons/"$1".icns
 }
 
 installAquaIcons()
 {
-cd ~/Downloads
 read -p "Find the target drive name in the list above (the one WITHOUT the - Data suffix), then enter the #s# values. Then, type them below: " diskNums
 sudo mount -o nobrowse -t apfs /dev/disk$diskNums ~/lvmnt
 
@@ -53,10 +54,51 @@ echo Downloading FileIcon icon changing utility...
 curl -O https://raw.githubusercontent.com/mklement0/fileicon/stable/bin/fileicon
 chmod +x fileicon 
 
+installSysIcon "App Store"
+installSysIcon "Automator"
+installSysIcon "Books"
+installSysIcon "Calculator"
+installSysIcon "Calendar"
+installSysIcon "Chess"
+installSysIcon "Clock"
+installSysIcon "Contacts"
+installSysIcon "Dictionary"
+installSysIcon "FaceTime"
+installSysIcon "Font Book"
+installSysIcon "Image Capture"
+installSysIcon "Launchpad"
+installSysIcon "Mail"
+installSysIcon "Messages"
+installSysIcon "Mission Control"
+installSysIcon "Music"
+installSysIcon "Notes"
+installSysIcon "Photo Booth"
+installSysIcon "Photos"
+installSysIcon "Podcasts"
+installSysIcon "Preview"
+installSysIcon "QuickTime Player"
+installSysIcon "Reminders"
+installSysIcon "Siri"
+installSysIcon "Stickies"
+installSysIcon "Stocks"
+installSysIcon "System Settings"
+installSysIcon "TextEdit"
+installSysIcon "Time Machine"
+installSysIcon "TV"
+installSysIcon "VoiceMemos"
+echo Installed icons for /System/Applications
 
+echo Installing Finder icon...
+sudo ./fileicon set ~/lvmnt/System/Library/CoreServices/Finder.app SystemIcons/Finder.icns
+sudo cp SystemIcons/finder.png /System/Library/CoreServices/Dock.app/Contents/Resources/
+sudo cp SystemIcons/finder@2x.png /System/Library/CoreServices/Dock.app/Contents/Resources/
+
+echo Installed Finder icon!
 
 sudo bless --mount ~/lvmnt --bootefi --create-snapshot
 sudo bless --mount "$HOME/lvmnt/System/Library/CoreServices/" --setBoot --create-snapshot > /dev/null 2>&1
+
+echo Finished installing! Now, reboot to apply.
 }
 
 
